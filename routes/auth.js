613d60ca762801629432bfcf9a553255bd6a41b1
ayowee <pasinduayomal2001@gmail.com>
2024-04-05 09:14:20 +0530

    Add logout functionality and header component

--- routes/auth.js
+++ routes/auth.js
@@ -12,6 +12,15 @@ router.get('/google/callback',
 passport.authenticate('google', { failureRedirect: '/' }),
 (req, res) => {
     res.redirect('/dashboard')
+}
+)
+
+//@desc Logout user
+//@route /auth/logout
+
+router.get('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/')
 })
 
 module.exports = router
