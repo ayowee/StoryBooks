5f0478a5ec0c3f9ad51697aa6672674cdd08bfc3
ayowee <pasinduayomal2001@gmail.com>
2024-04-05 07:16:36 +0530

    Add Google OAuth and Passport authentication

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 //Load config
 dotenv.config({path: './config/config.env'})
+
+//Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -17,8 +23,25 @@ if(process.env.NODE_ENV === 'development') {
 }
 
 //Handlebars
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+//Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}))
+
+//Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 5000;
 
---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+const express =require('express')
+const passport = require('../config/passport')
+const router = express.Router()
+
+
+// @desc  Auth with google
+// @router GET /auth/google
+router.get('/google', passport.authenticate('google', {scope: ['profile']}))
+
+// @desc  Google with Callback
+// @router GET /auth/google/callback 
+router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/'}),
+(req, res) => {
+    res.redirect('/dashboard')
+})
+
+module.exports = router
