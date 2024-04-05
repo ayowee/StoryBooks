aeea2242295bd67d8be2445c915c63b175a3ceb5
ayowee <pasinduayomal2001@gmail.com>
2024-04-05 08:51:11 +0530

    Add passport-google-oauth package and update dependencies

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,15 @@
 const express =require('express')
-const passport = require('../config/passport')
+const passport = require('passport')
 const router = express.Router()
-
 
 // @desc  Auth with google
 // @router GET /auth/google
-router.get('/google', passport.authenticate('google', {scope: ['profile']}))
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 // @desc  Google with Callback
 // @router GET /auth/google/callback 
-router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/'}),
+router.get('/google/callback', 
+passport.authenticate('google', { failureRedirect: '/' }),
 (req, res) => {
     res.redirect('/dashboard')
 })
