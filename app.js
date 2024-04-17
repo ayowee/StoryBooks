a5ee4cdae57615a0b9ffdea10ca600cad6ad99f5
ayowee <pasinduayomal2001@gmail.com>
2024-04-17 06:44:12 +0530

    Update Handlebars helpers, add select helper, and implement story edit functionality

--- app.js
+++ app.js
@@ -25,13 +25,23 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+// Method Override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+}))
+
 //logging
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
 //Handlebars Helpers
-const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
 
 //Handlebars
 app.engine('.hbs', exphbs.engine({ helpers: {
@@ -39,6 +49,7 @@ app.engine('.hbs', exphbs.engine({ helpers: {
             stripTags,
             truncate,
             editIcon,
+            select,
         }, 
         defaultLayout: 'main', 
         extname: '.hbs'
